ef06d7be684fdc7fda879f3613811fd66bff798c
amttgroup <58212446+amttgroup@users.noreply.github.com>
2019-11-29 15:04:43 +0800

    Update KrissTranslationTrait.php

--- traits/KrissTranslationTrait.php
+++ traits/KrissTranslationTrait.php
@@ -9,13 +9,13 @@ trait KrissTranslationTrait
     public function initKrissI18N()
     {
         $translations = Yii::$app->i18n->translations;
-        if (!isset($translations['kriss'])) {
+//         if (!isset($translations['kriss'])) {
             Yii::setAlias('@krissMessage', __DIR__ . '/../messages');
             Yii::$app->i18n->translations['kriss'] = [
                 'class' => 'yii\i18n\PhpMessageSource',
                 'basePath' => '@krissMessage',
                 'forceTranslation' => true,
             ];
-        }
+//         }
     }
 }
